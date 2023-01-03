0e0ab243c1efdbbd3105b2ceee029985a6bc9d76
Jeff Zhuo <jzhuo@unc.edu>
2023-01-02 22:43:47 -0500

    Gmail to SMS notification

---
+++ utils/gmail.js
@@ -0,0 +1,48 @@
+export default function sendMessage(message) {
+    const nodemailer = require("nodemailer");
+    const { google } = require("googleapis");
+    const OAuth2 = google.auth.OAuth2;
+
+    const clientID = 'Your ClientID Here';
+    const clientSecret = 'Your Client Secret Here';
+    const refreshToken = 'Your Refresh Token Here';
+    const user = 'Your Gmail Here';
+    const phoneGateway = 'phone-number@provider-gateway';
+
+    const oauth2Client = new OAuth2(
+        clientID, // ClientID
+        clientSecret, // Client Secret
+        "https://developers.google.com/oauthplayground" // Redirect URL
+    );
+
+    oauth2Client.setCredentials({
+        refresh_token: refreshToken
+    });
+    const accessToken = oauth2Client.getAccessToken()
+
+    const smtpTransport = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+            type: "OAuth2",
+            user: user, 
+            clientId: clientID,
+            clientSecret: clientSecret,
+            refreshToken: refreshToken,
+            accessToken: accessToken,
+            rejectUnauthorized: false
+        }
+    });
+
+    const mailOptions = {
+        from: user,
+        to: phoneGateway,
+        subject: "Class Status Update!",
+        generateTextFromHTML: true,
+        html: message
+    };
+
+    smtpTransport.sendMail(mailOptions, (error, response) => {
+        error ? console.log(error) : console.log(response);
+        smtpTransport.close();
+    });
+}
