baf1b7e780cdb104292fff5755973ac6ff020f73
Jeff Zhuo <jzhuo@unc.edu>
2023-01-04 00:26:48 -0500

    enhance btn, disable functionality

--- components/course/course.js
+++ components/course/course.js
@@ -1,8 +1,12 @@
 import styles from './course.module.css';
-
+import { useState } from 'react';
 export default function Course({ courseObj, btnTxt, clickHandler}) {
+    const [isDisable, setIsDisable] = useState(false);
+
     const onClick = async () => {
+        setIsDisable(true);
         await clickHandler(courseObj);
+        setIsDisable(false);
     }
 
     return(
@@ -14,7 +18,7 @@ export default function Course({ courseObj, btnTxt, clickHandler}) {
                 <p className={`btn my-auto align-self-center text-light ${courseObj.isClassOpen?`btn-success ${styles['btn-success']}`:`btn-danger ${styles['btn-danger']}`}`}>{courseObj.isClassOpen?'OPEN':'CLOSE'}</p>
             </div>
             <div className='my-1 col-sm-2'>
-                <button type='button' onClick={onClick} className='btn btn-warning'>{btnTxt}</button>
+                <button type='button' onClick={onClick} className='btn btn-warning' disabled={isDisable}>{btnTxt}</button>
             </div>
         </div>
     );
