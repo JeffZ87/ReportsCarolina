f29c40338456ffa46466c1025f6101fd74fe247f
Jeff Zhuo <jzhuo@unc.edu>
2023-01-04 02:45:07 -0500

    enhance responsiveness for course component

--- components/course/course.js
+++ components/course/course.js
@@ -10,15 +10,17 @@ export default function Course({ courseObj, btnTxt, clickHandler}) {
     }
 
     return(
-        <div className={`m-1 row border border-dark ${styles['bg-unc-color']}`}>
-            <div className='my-1 col-sm-6 d-flex'>
-                <p className='my-auto align-self-center'>{courseObj.subject} {courseObj.catalogNumber} - {courseObj.sectionNumber}</p>
-            </div>
-            <div className='py-1 col-sm-4 border-end border-start d-flex'>
-                <p className={`btn my-auto align-self-center text-light ${courseObj.isClassOpen?`btn-success ${styles['btn-success']}`:`btn-danger ${styles['btn-danger']}`}`}>{courseObj.isClassOpen?'OPEN':'CLOSE'}</p>
-            </div>
-            <div className='my-1 col-sm-2'>
-                <button type='button' onClick={onClick} className='btn btn-warning' disabled={isDisable}>{btnTxt}</button>
+        <div className='container-fluid p-0'>
+            <div className={`m-1 row border border-dark ${styles['bg-unc-color']}`}>
+                <div className='my-1 col-sm-12 col-lg-5 d-flex'>
+                    <p className='my-auto align-self-center'>{courseObj.subject} {courseObj.catalogNumber} - {courseObj.sectionNumber}</p>
+                </div>
+                <div className='py-1 col-sm-12 col-md-7 col-lg-5 border-dark border-end border-start d-flex justify-content-center'>
+                    <p className={`btn my-auto align-self-center text-light ${courseObj.isClassOpen?`btn-success ${styles['btn-success']}`:`btn-danger ${styles['btn-danger']}`}`}>{courseObj.isClassOpen?'OPEN':'CLOSE'}</p>
+                </div>
+                <div className='my-1 col-sm-12 col-md-5 col-lg-2 d-flex justify-content-center'>
+                    <button type='button' onClick={onClick} className='btn btn-warning align-self-center' disabled={isDisable}>{btnTxt}</button>
+                </div>
             </div>
         </div>
     );
